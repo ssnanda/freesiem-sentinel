666e8fe674b7704c83f5cc6e2c90b8b72328a944
sandman <ssn.jnk@gmail.com>
2026-05-07 21:24:36 -0400

    Release freeSIEM Sentinel 1.0.0

--- dist/freesiem-sentinel/freesiem-sentinel.php
+++ dist/freesiem-sentinel/freesiem-sentinel.php
@@ -3,7 +3,7 @@
  * Plugin Name: freeSIEM Sentinel
  * Plugin URI: https://github.com/ssnanda/freesiem-sentinel
  * Description: Connects a WordPress site to freeSIEM Core for verification, local scanning, secure result uploads, command handling, and summary reporting.
- * Version: 0.3.64
+ * Version: 1.0.0
  * Update URI: https://github.com/ssnanda/freesiem-sentinel
  * Author: freesiem.com
  * Text Domain: freesiem-sentinel
@@ -13,7 +13,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('FREESIEM_SENTINEL_VERSION', '0.3.64');
+define('FREESIEM_SENTINEL_VERSION', '1.0.0');
 define('FREESIEM_SENTINEL_SLUG', 'freesiem-sentinel');
 define('FREESIEM_SENTINEL_SYNCHY_PAGE', 'freesiem-synchy');
 define('FREESIEM_SENTINEL_OPTION', 'freesiem_sentinel_settings');
